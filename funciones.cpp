91dd90bf97eaab558127c0d0261a71486d923a23
Enrique <enrique.taleno16055@est.edu.ni>
2023-04-19 08:41:35 -0600

    fin

--- funciones.cpp
+++ funciones.cpp
@@ -8,7 +8,7 @@ int pos=0;
 
 void agregarEdad(int edad);
 bool hayEspacio();
-
+void mostrarEdades();
 void agregarEdad(int edad){
     edades[pos] = edad;
     pos++;
@@ -18,3 +18,9 @@ bool hayEspacio(){
     return pos < MAX;
 }
 
+void mostrarEdades(){
+    for (int i = 0; i < pos; i++)
+    {
+        cout << edades[i] << endl;
+    }
+}
--- principal.cpp
+++ principal.cpp
@@ -1,9 +1,29 @@
-#include<iostream>
-
+#include <iostream>
+#include "funciones.cpp"
 using namespace std;
 
-int main(int argc, char const *argv[])
+int main()
 {
-    /* code */
+    int cant, edad, i = 0;
+    cout << "cuantad edades desea ingresar: ";
+    cin >> cant;
+    while (i < cant)
+    {
+        cout << "Dime edad: ";
+        cin >> edad;
+        if (hayEspacio())
+            agregarEdad(edad);
+        else
+        {
+            cout << "no hay espacio sificiente para almacenar mas edades";
+            system("pause");
+            break;
+        };
+        agregarEdad(edad);
+        i++;
+    }
+    system("cls");
+    cout << "Imprimiendo edades:\n"; 
+    mostrarEdades();
     return 0;
 }
